fa3cca7faddf3b810775ff475503916a64e79e09
hengstchon <hengstchon@gmail.com>
2020-08-26 10:42:46 +0200

    update css

--- Search/themes/material/app.js
+++ Search/themes/material/app.js
@@ -767,7 +767,7 @@ function file_code(path) {
 	<label class="mdui-textfield-label">Download Link</label>
 	<input class="mdui-textfield-input" type="text" readonly value="${href}"/>
 </div>
-<a href="${href}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
+<a href="${href}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-${accent_color}"><i class="mdui-icon material-icons">file_download</i></a>
 <script src="https://cdn.staticfile.org/ace/1.4.7/ace.js"></script>
 <script src="https://cdn.staticfile.org/ace/1.4.7/ext-language_tools.js"></script>
 	`;
@@ -890,7 +890,7 @@ function file_audio(path) {
 	  <textarea class="mdui-textfield-input" readonly><audio><source src="${url}"></audio></textarea>
 	</div>
 </div>
-<a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
+<a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-${accent_color}"><i class="mdui-icon material-icons">file_download</i></a>
 	`;
   $("#content").html(content);
 }
@@ -904,7 +904,7 @@ function file_pdf(path) {
   );
   var content = `
 	<object data="${inline_url}" type="application/pdf" name="${file_name}" style="width:100%;height:94vh;"><embed src="${inline_url}" type="application/pdf"/></object>
-    <a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
+    <a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-${accent_color}"><i class="mdui-icon material-icons">file_download</i></a>
 	`;
   $("#content")
     .removeClass("mdui-container")
@@ -948,15 +948,15 @@ function file_image(path) {
                     <div class="mdui-col">
                         ${
                           prev_child
-                            ? `<button id="leftBtn" data-filepath="${prev_child}" class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple">Previous</button>`
-                            : `<button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple" disabled>Previous</button>`
+                            ? `<button id="leftBtn" data-filepath="${prev_child}" class="mdui-btn mdui-btn-block mdui-color-${accent_color} mdui-ripple">Previous</button>`
+                            : `<button class="mdui-btn mdui-btn-block mdui-color-${accent_color} mdui-ripple" disabled>Previous</button>`
                         }
                     </div>
                     <div class="mdui-col">
                         ${
                           next_child
-                            ? `<button id="rightBtn"  data-filepath="${next_child}" class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple">Next</button>`
-                            : `<button class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple" disabled>Next</button>`
+                            ? `<button id="rightBtn"  data-filepath="${next_child}" class="mdui-btn mdui-btn-block mdui-color-${accent_color} mdui-ripple">Next</button>`
+                            : `<button class="mdui-btn mdui-btn-block mdui-color-${accent_color} mdui-ripple" disabled>Next</button>`
                         }
                     </div>
                 </div>
@@ -990,7 +990,7 @@ function file_image(path) {
 	</div>
         <br>
 </div>
-<a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
+<a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-${accent_color}"><i class="mdui-icon material-icons">file_download</i></a>
     `;
   //my code
   $("#content").html(content);
